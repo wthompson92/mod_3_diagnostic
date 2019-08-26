bdb75eea3283c79d1d18a0bc17c323c116bd901f
username <wnt925@gmail.com>
2019-08-26 10:59:08 -0600

    last commit

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -9,7 +9,7 @@ class NrelService
 private
   def conn
     @_conn = Faraday.new(:url => 'https://developer.nrel.gov/docs/transportation/alt-fuel-stations-v1/nearest/') do |faraday|
-    faraday.headers['api_key'] = '4Z1qtSr1b6WD6o4MUcWH0J4SowKe65NIaaG68K1B'
+    faraday.headers['api_key'] = 'removes'
     faraday.params['zip'] = 80501
     faraday.params['limit'] = 10
     faraday.adapter  Faraday.default_adapter
