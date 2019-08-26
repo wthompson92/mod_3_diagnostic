21c59c4e0b192db0e6df07720ad01659057467c7
username <wnt925@gmail.com>
2019-08-26 09:28:37 -0600

    Initial Commit

---
+++ spec/api/v1/search/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+
+feature "User can search for nearby gas stations by zip code" do
+  scenario "When I visit the root path and  I fill in the search form with 80202, Then I should see a list of the 10 closest stations within 6 miles sorted by distance" do
+    visit "/"
+      fill_in :search, with: '80501'
+      expect(current_path).to eq(search_path)
+      end
+
+  scenario  "And the stations should be limited to Electric and Propane. And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times " do
+  end
+end
